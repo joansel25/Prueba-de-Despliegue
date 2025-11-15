3972b7b13e1aba3bbbe1621fec7093a6e359613b
joansel25 <joan.cardenas@correo.tdea.edu.co>
2025-11-15 01:57:05 -0500

    2 version de despliegue back django

--- Inventario-Django/farmacia/farmacia/settings.py
+++ Inventario-Django/farmacia/farmacia/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-nhl#v+oclsu8o^7rl7i4dg7gr@$rt46s1yib*%s(_yhv0^6eu-'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+
+DEBUG = False
 # DEBUG = True
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", '127.0.0.1', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = [".vercel.app", ".now.sh", '127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -159,10 +159,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-# En settings.py agregar:
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

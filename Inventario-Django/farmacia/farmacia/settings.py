b762225ee08b7b487f107db7317a2e9fbbd6dfd9
joansel25 <joan.cardenas@correo.tdea.edu.co>
2025-11-17 21:38:58 -0500

    Agregando URL en Cors del Frontend

--- Inventario-Django/farmacia/farmacia/settings.py
+++ Inventario-Django/farmacia/farmacia/settings.py
@@ -182,7 +182,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    'https://prueba-de-despliegue-wyj1.onrender.com',   
+    'https://prueba-de-despliegue-wyj1.onrender.com',
+    'https://frontend-inventory-farmacia.netlify.app/',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
